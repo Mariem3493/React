ea01044c6cced57ef64f674974b177d961d2a75d
Mariem3493 <mariem.ouechtati93@gmail.com>
2023-05-29 21:21:30 +0200

    React

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Carousell from './Composant/Carousell';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulo from './Composant/Formulo';
+function App() {
+  return (
+    <div>
+      <Carousell/>
+      <Formulo/>
+    </div>
+  )
+}
+
+export default App;
---
+++ myapp/src/Composant/Carousell.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import image1 from '../Assets/1.jpg';
+import image2 from '../Assets/2.jpg';
+import image3 from '../Assets/3.jpg';
+function Carousell(){
+    return (
+        <div>
+            <h1 style={{textAlign : "center", color : "red"}}>Hello JSX</h1>
+            <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </div>
+    )   
+}
+export default Carousell;
