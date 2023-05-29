a7ad1d3940ae9d3e7503d465661e5b0f232c5d72
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-05-29 21:45:49 +0200

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Carousell from './Composant/Carousell';
+
+{/* importation de notre stylesheet boostrap */}
 import 'bootstrap/dist/css/bootstrap.min.css';
+{/* importation du Carousell */}
+import Carousell from './Composant/Carousell';
+{/* importation de notre Formulaire */}
 import Formulo from './Composant/Formulo';
+{/* Function qui retourne nos composants */}
 function App() {
   return (
     <div>
+    {/*appel de nos composants */}
+
       <Carousell/>
       <Formulo/>
     </div>
